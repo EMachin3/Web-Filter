3ad68553412c18086e0d3cd0287a7bcdc0faf1ba
EMachin3 <95111563+EMachin3@users.noreply.github.com>
2023-08-17 19:48:14 -0400

    Add persistent local storage and updating filters by pressing enter.

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 //let filters = ["\"free\""]
 //TODO: Restore the old page contents if something isn't being filtered anymore
-let filters = []
-filterWebpage();
+let filters = [];
+browser.storage.local.get("storedFilters", function (result) {
+    alert(JSON.stringify(result));
+    // if (Object.keys(result).length === 0) {
+    //     alert('There is no key!'); 
+    //     filters = [];
+    // }
+    // else {
+    //     filters = result[]
+    // }
+    if (Object.keys(result).length !== 0) {
+        filters = result["storedFilters"];
+    }
+    //filters = (Object.keys(result).length === 0) ? [] : result["storedFilters"];
+})
+filterWebpage()
 window.setInterval(filterWebpage, 10000);
 browser.runtime.onMessage.addListener((message) => {
     if (message.command === "updateFilters") {
@@ -19,6 +33,7 @@ function updateFilters(csvFilters) {
     newFilterList.push("\"" + filtersList[filtersList.length - 1] + "\"")
     alert(newFilterList.join(""));
     filters = newFilterList;
+    browser.storage.local.set({"storedFilters": newFilterList});
 }
 //TODO: Function for updating popup to state which phrases are currently filtered
 function filterWebpage() {
--- popup/configure_filter.js
+++ popup/configure_filter.js
@@ -2,19 +2,25 @@
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
  */
-function listenForClicks() {
-  document.addEventListener("click", (e) => {
-    if (e.target.tagName !== "BUTTON" || !e.target.closest("#updateFiltersButton")) {
-      return;
-    }
-    browser.tabs.query({}, tabs => {
-      tabs.forEach(tab => {
-        browser.tabs.sendMessage(tab.id, {
-          command: "updateFilters",
-          updatedFilters: document.getElementById("filters").value
-        });
+document.addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON" || !e.target.closest("#updateFiltersButton")) {
+    return;
+  }
+  sendUpdateMessage();
+});
+const inputField = document.getElementById("filters");
+inputField.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    sendUpdateMessage();
+  }
+});
+function sendUpdateMessage() {
+  browser.tabs.query({}, tabs => {
+    tabs.forEach(tab => {
+      browser.tabs.sendMessage(tab.id, {
+        command: "updateFilters",
+        updatedFilters: document.getElementById("filters").value
       });
     });
   });
-}
-listenForClicks() //TODO: Either add more functionality to this file or make this a lambda expression
+};
